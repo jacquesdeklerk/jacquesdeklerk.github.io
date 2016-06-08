20a14f17de8f32ec9f7ccb066e2a6a110391143a
Jacques de Klerk <jacques.de.klerk@gmail.com>
2016-06-08 21:44:17 +0200

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+var gulp = require('gulp');
+var gulpLoadPlugins = require('gulp-load-plugins');
+
+
+var buildControl = require('build-control').BuildControl;
+
+var bc = new buildControl({
+  branch: 'dist',
+  dir: './build/bundled',
+  remote: {
+   repo: 'git@github.com:jacquesdeklerk/jacquesdeklerk.github.io.git',
+   branch: 'master'
+ },
+});
+
+var $ = gulpLoadPlugins();
+
+
+
+gulp.task('deploy', () => {
+  return bc.run();
+});
